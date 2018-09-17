bb80eb49c5bcc29cf4474ca1b11899c5cc98c73e
jeevangowda <42933086+jeevangowda@users.noreply.github.com>
2018-09-17 15:41:21 +0530

    Add files via upload

---
+++ grads.py
@@ -0,0 +1,16 @@
+num=int(input("enter the marks"))
+if(num>=90):
+	print("grade A+")
+elif(num>=80):
+	print("grade A")
+elif(num>=70):
+	print("grade B+")
+elif(num>=60):
+	print("grade B")
+elif(num>=50):
+	print("grade C")
+elif(num>=40):
+	print("grade D")
+else:
+	print("fail")
+	#to check the grade
