eb904577e458530d9e60c8cbea424a3f5f073d93
jeevangowda <42933086+jeevangowda@users.noreply.github.com>
2018-09-17 16:17:29 +0530

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,13 @@
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+if(a>b&a>c):
+	print("a is greater")
+elif(b>c):
+	print("b is greater")
+else:
+	print("c is greater")
+
+
+
+	# greatest of 3 number 
---
+++ greater2.py
@@ -0,0 +1,9 @@
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+if(a>b):
+	print("a is greater")
+elif(b>a):
+	print("b is greater")
+else:
+	print("both are equal")
+	# greatest of 2 number
